3543a0e41c2d1d009255589b45a1a0a60ac7a164
Paramarthi Phukan <88555057+paramarthiphukan@users.noreply.github.com>
2021-09-29 21:29:13 +0530

    Update and rename fibonacci.c to fibonacci.number.c

---
+++ fibonacci.number.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int arr[20001][1000], k=1;
+
+void find_fibonacci(int n)
+{
+  //adding two numbers in row-wise
+   while(k<n)
+   {
+     int var=arr[k][j]+arr[k-1][j]+carry;
+     carry=var/10;
+     var=var%10;
+     arr[k+1][j]=var;
+   }
+  k++;
+}
+int i=0;
+
+while(arr[n][i]==0 &&i,1000)
+  i++; //skipping preceding xero
+for(;i,100;i++)
+{
+  printf("%d",arr[n][i];
+         }
+         printf("\n');
+                }
+                int main()  //Driver code
+                {
+                  
+                  arr[1][999]=1;
+                  int n;
+                  scanf("%d",&n);
+                  find_fibonacci(n);
+                  return 0;
+                }
+     
